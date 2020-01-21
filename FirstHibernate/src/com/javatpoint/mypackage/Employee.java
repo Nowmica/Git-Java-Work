3e59807d2746bef8b59d0976f83f777abf67ec2a
Nowmica <57382951+Nowmica@users.noreply.github.com>
2020-01-21 12:49:00 +0530

    Add files via upload

---
+++ FirstHibernate/src/com/javatpoint/mypackage/Employee.java
@@ -0,0 +1,33 @@
+package com.javatpoint.mypackage;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee 
+{
+	
+@Id
+	private String firstName,lastName; 
+	private int id;  
+	
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+	
+}
